2b44b5f0eb91bed14d2832c23fc5c0bd5d486170
BORBECH <charlieborbs18@gmail.com>
2020-10-16 13:58:55 +0800

    [ParkingLot][Charlie] test : Create a test that return a parking ticket when a parking boy parks a car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,13 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public ParkingTicket park(Car car) {
+        return null;
+    }
 }
