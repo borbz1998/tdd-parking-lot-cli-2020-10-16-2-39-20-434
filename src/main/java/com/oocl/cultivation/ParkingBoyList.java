aaf139b8f5601e9744f30b8d70402c4d347055d6
BORBECH <charlieborbs18@gmail.com>
2020-10-17 22:52:32 +0800

    [ParkingLot][Charlie] feature : Create a new ParkingBoyList that can store all ParkingBoyTypes (ParkingBoy, SmartParkingBoy,  SuperSmartParkingBoy, ParkingLotManager)

--- src/main/java/com/oocl/cultivation/ParkingBoyList.java
+++ src/main/java/com/oocl/cultivation/ParkingBoyList.java
@@ -1,4 +1,19 @@
 package com.oocl.cultivation;
 
+import com.oocl.cultivation.Interface.IParkingBoy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ParkingBoyList {
+    private List<IParkingBoy> parkingBoyList = new ArrayList<>();
+
+    public ParkingBoyList(IParkingBoy... parkingBoyList) {
+        this.parkingBoyList.addAll(Arrays.asList(parkingBoyList));
+    }
+
+    public List<IParkingBoy> getParkingBoyList() {
+        return parkingBoyList;
+    }
 }
