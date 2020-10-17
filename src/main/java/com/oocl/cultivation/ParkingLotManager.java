90d8b1b1856e01cf041100f39b4c4abef234e5e3
BORBECH <charlieborbs18@gmail.com>
2020-10-17 21:51:05 +0800

    [ParkingLot][Charlie] feature : Implement methods and inherit ParkingBoyBehavior in ParkingLotManager class

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -1,4 +1,19 @@
 package com.oocl.cultivation;
 
-public class ParkingLotManager {
+import com.oocl.cultivation.Behavior.ParkingBoyBehavior;
+
+public class ParkingLotManager extends ParkingBoyBehavior {
+
+    public ParkingLotManager(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
+
+    @Override
+    public ParkingTicket park(Car car) {
+        return super.park(car);
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return parkingLot.fetch(parkingTicket, parkingLot.getParkingLotMapLists());
+    }
 }
