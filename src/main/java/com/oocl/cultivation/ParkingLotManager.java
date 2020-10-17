1197ed0bd404d632e27ecbde0a1cc28dd5f90fa0
BORBECH <charlieborbs18@gmail.com>
2020-10-17 22:54:17 +0800

    [ParkingLot][Charlie] feature : Implement ParkingLotManager wherein he/she can add new ParkingBoy in the ParkingBoyList

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -1,8 +1,14 @@
 package com.oocl.cultivation;
 
 import com.oocl.cultivation.Behavior.ParkingBoyBehavior;
+import com.oocl.cultivation.Interface.IParkingBoy;
 
-public class ParkingLotManager extends ParkingBoyBehavior {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingLotManager extends ParkingBoyBehavior implements IParkingBoy {
+    private ParkingBoyList parkingBoyList;
+    private List<IParkingBoy> listOfParkingBoy;
 
     public ParkingLotManager(ParkingLot parkingLot) {
         super(parkingLot);
@@ -15,5 +21,10 @@ public class ParkingLotManager extends ParkingBoyBehavior {
 
     public Car fetch(ParkingTicket parkingTicket) {
         return parkingLot.fetch(parkingTicket, parkingLot.getParkingLotMapLists());
+    }
+
+    public List<IParkingBoy> addNewParkingBoy(ParkingBoyList parkingBoyList, IParkingBoy newParkingBoy){
+        parkingBoyList.getParkingBoyList().add(newParkingBoy);
+        return parkingBoyList.getParkingBoyList();
     }
 }
