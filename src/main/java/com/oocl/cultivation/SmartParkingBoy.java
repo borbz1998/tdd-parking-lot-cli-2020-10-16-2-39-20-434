660b7f1bd32a755bc832a6b00f540a678e1e9ba8
BORBECH <charlieborbs18@gmail.com>
2020-10-17 20:54:16 +0800

    [ParkingLot][Charlie] refactor : Refactor and remove unused methods and variables in SmartParkingBoy and inherits SmartParkingBoyBehavior

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,10 +1,10 @@
 package com.oocl.cultivation;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import com.oocl.cultivation.Behavior.SmartParkingBoyBehavior;
+
 import java.util.List;
 
-public class SmartParkingBoy extends ParkingBoy {
+public class SmartParkingBoy extends SmartParkingBoyBehavior {
     private List<Integer> parkingLotMapEmptyPosition;
     private int index = 0;
 
@@ -13,44 +13,12 @@ public class SmartParkingBoy extends ParkingBoy {
     }
 
     @Override
-    public List<ParkingLot> getParkingLotList() {
-        return super.getParkingLotList();
-    }
-
     public ParkingTicket park(Car car) {
-        parkingLotMapEmptyPosition = getTheParkingLotWithMoreEmptyPosition(parkingLot.getParkingLotMapLists(), index);
-        int maxParkingLotSpaceIndex = parkingLotMapEmptyPosition.indexOf(Collections.max(parkingLotMapEmptyPosition));
-        return parkingLot.park(car, parkingLot.getParkingLotMapLists().get(maxParkingLotSpaceIndex).getParkingLotMap());
+        return super.park(car);
     }
 
-    @Override
     public Car fetch(ParkingTicket parkingTicket) {
-        return super.fetch(parkingTicket);
+        return parkingLot.fetch(parkingTicket, parkingLot.getParkingLotMapLists());
     }
 
-    public List<Integer> getTheParkingLotWithMoreEmptyPosition(List<ParkingLot> parkingLotMapLists, int index) {
-        List<Integer> availableSpaceList = new ArrayList<>();
-        for (ParkingLot parkingLotMapList : parkingLotMapLists) {
-            availableSpaceList.add(parkingLotMapLists.get(index).getParkingLotCapacity() - parkingLotMapList.getParkingLotMap().size());
-            index++;
-        }
-        if (Collections.frequency(availableSpaceList, 0) == availableSpaceList.size()) {
-            throw new NoParkingLotSpaceException();
-        }
-        return availableSpaceList;
-    }
-
-    public String getCurrentLocation(List<ParkingLot> parkingLotLists, ParkingTicket parkingTicket) {
-        String currentLocation = "";
-        List<ParkingTicket> position;
-        for (ParkingLot parkingLot : parkingLotLists) {
-            if (parkingLot.getParkingLotMap().containsKey(parkingTicket)) {
-                position = new ArrayList<ParkingTicket>(parkingLot.getParkingLotMap().keySet());
-//                currentLocation += "ParkingLot Number: " + (index+1) + ", Position: " + (position.indexOf(parkingTicket)+1);
-                currentLocation += "ParkingLot Number: " + (index+1);
-            }
-            index++;
-        }
-        return currentLocation;
-    }
 }
