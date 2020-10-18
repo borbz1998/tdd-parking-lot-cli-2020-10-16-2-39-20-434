2f5954aec73999779dc6dc0d025f93e58f9272df
BORBECH <charlieborbs18@gmail.com>
2020-10-18 17:54:44 +0800

    [ParkingLot][Charlie] feature : Create new ParkingLotList in checking whether Parking Boy is assigned to that Parking Lot

--- src/main/java/com/oocl/cultivation/ParkingLotList.java
+++ src/main/java/com/oocl/cultivation/ParkingLotList.java
@@ -1,10 +1,24 @@
 package com.oocl.cultivation;
 
+import com.oocl.cultivation.Interface.IParkingBoy;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParkingLotList {
     private ParkingLot parkingLot;
 
-    private List<ParkingLot> parkingLotList;
+    private Map<IParkingBoy, List<ParkingLot>> parkingBoyParkingLotMap = new HashMap<>();
 
+    public ParkingLotList() {
+    }
+
+    public Map<IParkingBoy, List<ParkingLot>> getParkingBoyParkingLotMap() {
+        return parkingBoyParkingLotMap;
+    }
+
+    public void setParkingBoyParkingLotMap(Map<IParkingBoy, List<ParkingLot>> parkingBoyParkingLotMap) {
+        this.parkingBoyParkingLotMap = parkingBoyParkingLotMap;
+    }
 }
